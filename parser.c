66a565547529f1d102241cda58e556064f097b21
CWhatIDidThere <robin147@seznam.cz>
2018-11-20 18:00:37 +0100

    Parser update - Přidány komentáře - Změněna logika u <main>-><stat> - Opraveny některé chyby

--- parser.c
+++ parser.c
@@ -9,8 +9,6 @@
 
 void parse_main();
 
-void parse_statement();
-
 void parse_function();
 
 void parse_st_list(int position_helper);
@@ -21,96 +19,58 @@ void parse_param_list_1();
 
 void parse_param_list_2();
 
-void parse_arg();
-
-void parse_arg_list(int token);
+void parse_arg(int token_type);
 
 void parse_arg_list2();
 
-void parse_arg_list_b();
-
-void parse_arg_list2_b();
+void parse_arg_list2b();
 
 void parse_arg_list_switcher();
 
 int precedence_table_fake(int fake);
 
 Token token;
-int i = 0;
+
+
+//Changes old pointer value to actual token and gets new token for actual token
+void parse_old_token(){
+
+}
 
 //Parse for <main> LL
 void parse_main() {
     token = getToken();
 
     switch (token.type) {
+
         case KW_DEF://3
             parse_function();
             break;
-        case T_IDENTIFIER:// 11 12 16 17 27
-            token = getToken();
-            switch (token.type) {
-                case OP_EQ://16 27
-                    token = getToken();
-                    if (token.type == T_IDENTIFIER) {//27 -> 28 29
-                        parse_arg_list_switcher();//TODO Check if working
-                    } else {//16
-                        (precedence_table_fake(token.type)) ? exit(4) : 0;//TODO Legit?
-                        //TODO EXPR - PRECEDENCE TABLE + GIVE TOKEN + CHECK EXIT CODE
-                    }
-                    break;
-                case T_EOL:// 17
-                    break;
-                default://11 12
-                    parse_arg_list_switcher();
-                    break;
-            }
 
+        case T_EOL:
             break;
-        case T_EOL:// 6
-            break;
-        case KW_IF:// 19
-            token = getToken();
-            (precedence_table_fake(token.type)) ? exit(4) : 0;
-            //TODO Will precedence give me/steal token? if y, change this
-            token = getToken();
-            if ((token.type == KW_THEN) && (getToken().type) == T_EOL) {
-                parse_st_list(0);//TODO Check if working
-                parse_st_list(2);
-            } else {
-                exit(2);
-            }
-            break;
-        case KW_WHILE:// 24
-            token = getToken();
-            (precedence_table_fake(token.type)) ? exit(4) : 0;
-            token = getToken();
-            if ((token.type == KW_DO) && (getToken().type) == T_EOL) {
-                parse_st_list(2);
-                if ((getToken().type) != T_EOL) {
-                    exit(2);
-                }
-            } else {
-                exit(2);
-            }
-            break;
-        case T_EOF: // 7 TODO End of EOL, check SEM Analysis etc.
+
+        case T_EOF: // 7 TODO End of INPUT, check SEM Analysis etc.
             exit(0);
+
+            //Unique options for <main> checked, now goes into <stat> to check rest
         default :
-            token = getToken();
-            (precedence_table_fake(token.type)) ? exit(4) : 0;
+            parse_st_list(4);
             break;
-    }//TODO Cases for functions (inputs,..._)
-    parse_main();//Calling myself, cycle
+    }
+    parse_main();//Calling myself, stopped by T_EOF or error EXIT
 }
 
+//Parse for <func> LL
 void parse_function() {//3
 
     token = getToken();
 
     if ((token.type == T_IDENTIFIER) && (getToken().type) == T_LBRACKET) {
+        //Call function for <param-l>
         parse_param_list_1();
         if ((getToken().type) == T_EOL) {
-            i++;
+            //Call function for <st-list>
             parse_st_list(2);//TODO Quick fixed, check the logic
         } else {
             exit(2);
@@ -120,43 +80,58 @@ void parse_function() {//3
     }
 }
 
-void parse_st_list(int position_helper) {//TODO implement position_helper properly
+//Parse for <st-list> LL
+//position_helper used for check if get new token and go back to main(4) and if its in if(0) or in else(2) part
+void parse_st_list(int position_helper) {
 
-    token = getToken();
+    if (position_helper != 4) {//TODO Change to bool check or?
+        token = getToken();
+    }
+
     switch (token.type) {
+
         case T_IDENTIFIER:// 16 17 27
             token = getToken();
+
             switch (token.type) {
+
                 case OP_ASS://16 27
                     token = getToken();
-                    if (token.type == T_IDENTIFIER) {//27 -> 28 29 !TRAP! I do not know if its function or identifier
 
+                    if (token.type == T_IDENTIFIER) {//27 -> 28 29 !TRAP! I do not know if its function or identifier
+                        token = getToken();
                         parse_arg_list_switcher();
                     } else {//16
                         (precedence_table_fake(token.type)) ? exit(4) : 0;
                         token = getToken();
                     }
-                    if(token.type != T_EOL){
-                        exit(2);
-                    }
-                    if(position_helper == 0){
+                    /* if (token.type != T_EOL) {//Solved by precedence right?
+                         exit(2);
+                     }*/
+                    //If position_helper is 4, which means its call from main, it goes back
+                    if (position_helper == 0) {
                         parse_st_list(0);
-                    }else{
+                    } else if (position_helper == 2){
                         parse_st_list(2);
                     }
                     break;
+
                 case T_EOL:// 17
                     break;
-                default:
-                    exit(2);
+
+                default://11 12
+                    parse_arg_list_switcher();
+                    break;
             }
             break;
-            /*case T_EOL:// 6 //TODO Legit???
-                break;*/
+
+        case T_EOL:// 6 TODO Check this if legit both main and stat returning
+            parse_st_list(position_helper);
+            break;
+
         case KW_IF:// 19
             token = getToken();
             (precedence_table_fake(token.type)) ? exit(4) : 0;
-            //TODO Will precedence give me/steal token? if y, change this
             token = getToken();
             if ((token.type == KW_THEN) && (getToken().type) == T_EOL) {
                 parse_st_list(0);
@@ -165,6 +140,7 @@ void parse_st_list(int position_helper) {//TODO implement position_helper proper
                 exit(2);
             }
             break;
+
         case KW_WHILE:// 24
             token = getToken();
             (precedence_table_fake(token.type)) ? exit(4) : 0;
@@ -178,31 +154,51 @@ void parse_st_list(int position_helper) {//TODO implement position_helper proper
                 exit(2);
             }
             break;
+
         case KW_ELSE:
-            if(position_helper != 0){
+            if (position_helper != 0) {
                 exit(2);
-            }else if(getToken().type != T_EOL){
+            } else if (getToken().type != T_EOL) {
                 exit(2);
             }
             break;
+
         case KW_END:
-            if(position_helper <= 0){
+            if (position_helper <= 0) {
                 exit(2);
-            }else if(getToken().type != T_EOL){
+            } else if (getToken().type != T_EOL) {
                 exit(2);
             }
             break;
+
+            //BIF Handling
+        case BIF_INPUTS://32
+        case BIF_INPUTI://33
+        case BIF_INPUTF://34
+        case BIF_SUBSTR://38
+        case BIF_ORD://39
+        case BIF_CHR://40
+            break;//TODO Think this up
+
+        case BIF_LENGTH://37
+            parse_arg_list_switcher();//TODO Test if legit
+            break;
+
+        case BIF_PRINT://35
+            break;//TODO Think this up
+
         default:
             exit(2);
     }
-    i--;
 }
 
+//Parse for <param-l> LL
 void parse_param_list_1() {//58
     parse_param();
     parse_param_list_2();
 }
 
+//Parse for <param-l2> LL
 void parse_param_list_2() {
     token = getToken();
     if (token.type == T_COMMA) {//59
@@ -213,6 +209,7 @@ void parse_param_list_2() {
     }//60
 }
 
+//Parse for <param> LL
 void parse_param() {//61
     token = getToken();
     if (token.type != T_IDENTIFIER) {
@@ -220,20 +217,31 @@ void parse_param() {//61
     }
 }
 
+//Function for choosing, if send it to <arg-list2> or <arg-list2b>
 void parse_arg_list_switcher() {
-    token = getToken();
+    //token=getToken();// 19.11 ?
     if (token.type == T_LBRACKET) {
-        parse_arg_list_b();
-        parse_arg_list2_b();
+        //Solves <arg-listb> LL
+        token = getToken();
+        parse_arg(token.type);//47
+        //Call function for <arg-list2b>
+        parse_arg_list2b();//44,45
+
         if (getToken().type != T_EOL) {
             exit(2);//TODO .. Legit?
         }
-    } else if(token.type != T_EOL) {
-        parse_arg_list(token.type);
+
+    } else if (token.type != T_EOL) {
+        //Solves <arg-list> LL
+        parse_arg(token.type);//43
+        //Call function for <arg-list2>
+        parse_arg_list2();//48,49
     }
 }
 
+//Parse for <arg> LL
 void parse_arg(int token_type) {
+    //TODO Make this one better / Checks if token is taken or should take
     if (token_type == 399) {
         token = getToken();
     }
@@ -245,20 +253,15 @@ void parse_arg(int token_type) {
         case T_STRING://54
             //free(token.data);//TODO Check if free is legit
             break;
-        default://TODO For real like this? Check
+        default:
             exit(2);
     }
 }
 
-
-void parse_arg_list(int token_type) {//43
-    parse_arg(token_type);
-    parse_arg_list2();
-}
-
+//Parse for <arg-list2> LL
 void parse_arg_list2() {
     token = getToken();
-    if (token.type == T_COMMA) {//48
+    if (token.type == T_COMMA) {//44
         parse_arg(399);
         parse_arg_list2();
     } else if (token.type != T_EOL) {//45
@@ -266,26 +269,23 @@ void parse_arg_list2() {
     }
 }
 
-void parse_arg_list_b() {//47
-    token = getToken();
-    parse_arg(token.type);
-    parse_arg_list2();
-}
-
-void parse_arg_list2_b() {
+//Parse for <arg-list2b> LL
+void parse_arg_list2b() {
     token = getToken();
     if (token.type == T_COMMA) {//48
         parse_arg(399);
-        parse_arg_list2_b();
+        parse_arg_list2b();
     } else if (token.type != T_RBRACKET) {//49
         exit(2);
     }
 }
 
+//Just for testing purposes, will be replaced by real precedence table
 int precedence_table_fake(int fake) {
     return 0;
 }
 
+//Just main, nothing special
 int main() {
     parse_main();
     return 0;
