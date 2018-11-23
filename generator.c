c6c20e0c35a599e102df272bcb445572682902e7
vojtech-novotny <31734119+vojtech-novotny@users.noreply.github.com>
2018-11-23 17:46:53 +0100

    Added new functions
    Přidal jsem další generační funkce.

--- generator.c
+++ generator.c
@@ -7,8 +7,22 @@
 
 #include "generator.h"
 
-inline int gen_parameter(Token token, int cisloParametru) {
-    printf("DEFVAR TF@%d\nMOVE TF@%d ", cisloParametru, cisloParametru);
+/// Generates code for 1 function argument.
+/// token           Contains the token that is used as the argument.
+/// argumentNumber  Contains the position of the argument.
+///                     Send positive number to set the last position to a positive number.
+///                     Send GEN_COUNTER_ADD to increment the last used number and use it as position number.
+inline void gen_argument(Token token, int argumentNumber) {
+
+    static int argumentCounter = 0;
+
+    if (argumentNumber == GEN_COUNTER_ADD)
+        argumentCounter++;
+    else
+        argumentCounter = argumentNumber;
+
+
+    printf("DEFVAR TF@%d\nMOVE TF@%d ", argumentNumber, argumentNumber);
 
     switch (token.type) {
         case T_IDENTIFIER:
@@ -23,19 +37,40 @@ inline int gen_parameter(Token token, int cisloParametru) {
             free(token.data);
             break;
         case T_STRING:
-            printf("int@%s\n", (char*)token.data);
+            printf("int@%s\n", (char*)(token.data));
             free(token.data);
             break;
         default:
             printf("WHEEE\n");
             break;
     }
-
-    return 0;
 }
 
-inline int gen_TF() {
+/// Generates code to create new temporary frame.
+inline void gen_TF() {
     printf("CREATE FRAME\n");
+}
 
-    return 0;
+/// Generates code to define a new label.
+/// token           token.data character string is used as the name for the label
+inline void gen_label(Token token) {
+    printf("LABEL $%s\n", (char*)token.data);
+}
+
+
+inline void gen_parameter(Token token, int parameterNumber) {
+
+    static int parameterCounter = 0;
+
+    if (parameterNumber == GEN_COUNTER_ADD)
+        parameterCounter++;
+    else
+        parameterCounter = parameterNumber;
+
+    printf("DEFVAR LF@%s\nMOVE LF@%s LF@%d ", (char*)(token.data), (char*)(token.data), parameterNumber);
+}
+
+/// Generates code for return value variable definition.
+inline void get_retval() {
+    printf("DEFVAR LF@%retval\nMOVE LF@%retval nil@nil");
 }
--- generator.h
+++ generator.h
@@ -13,8 +13,30 @@
 #ifndef GEN_H
 #define GEN_H
 
-inline int gen_parameter(Token token, int cisloParametru);
+#define GEN_COUNTER_ADD -1
 
-inline int gen_TF();
+/// Generates code for 1 function argument.
+/// token           Contains the token that is used as the argument.
+/// argumentNumber  Contains the position of the argument.
+///                     Send positive number to set the last position to a positive number.
+///                     Send GEN_COUNTER_ADD to increment the last used number and use it as position number.
+inline void gen_argument(Token token, int argumentNumber);
+
+/// Generates code to create new temporary frame.
+inline void gen_TF();
+
+/// Generates code to define a new label.
+/// token           token.data character string is used as the name for the label
+inline void gen_label(Token token);
+
+/// Generates code for 1 function parameter.
+/// token           Contains the token that is used as the parameter.
+/// parameterNumber  Contains the position of the parameter.
+///                     Send positive number to set the last position to a positive number.
+///                     Send GEN_COUNTER_ADD to increment the last used number and use it as position number.
+inline void gen_parameter(Token token, int parameterNumber);
+
+/// Generates code for return value variable definition.
+inline void get_retval();
 
 #endif // !GEN_H
